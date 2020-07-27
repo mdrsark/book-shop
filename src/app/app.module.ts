cac103c242f5968ffcf8c8b666759c905ea2c0f7
Daniel Rey <daniel.rey@wnco.com>
2020-07-27 11:37:29 -0500

    Adding new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContainerComponent } from './components/container/container.component';
+import { FloatingActionComponent } from './components/floating-action/floating-action.component';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+    FloatingActionComponent,
+    CardComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/floating-action/floating-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-floating-action',
+  templateUrl: './floating-action.component.html',
+  styleUrls: ['./floating-action.component.css']
+})
+export class FloatingActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
